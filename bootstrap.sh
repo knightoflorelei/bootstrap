f42d34d4a48346f383b83fbaad43ae19fec2702e
knightoflorelei <knightoflorelei@gmail.com>
2022-09-25 08:52:17 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,5 +14,5 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 sudo dnf install gnome-tweak &&
 
 # Install neovim
-sudo dnf install neovim &&
+sudo dnf install neovim
 
