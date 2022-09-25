f30e06052dd856c7182263f9d182277a56bb71fb
knightoflorelei <knightoflorelei@gmail.com>
2022-09-25 14:13:10 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,9 +12,20 @@ sudo dnf groupupdate core &&
 # Enable Flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
 
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+
 # Install Gnome Tweaks
 sudo dnf install gnome-tweaks &&
 
 # Install neovim
-sudo dnf install neovim
+sudo dnf install neovim &&
 
+# Install zsh
+sudo dnf install zsh &&
+
+# Install Homebrew packages
+brew install lf mpd mpv scrcpy powerlevel10k ranger zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting &&
+
+# Install flatpak applications
+flatpak install com.brave.Browser org.gnome.gitlab.somas.Apostrophe io.github.celluloid_player.Celluloid com.mattjakeman.ExtensionManager com.discordapp.Discord
