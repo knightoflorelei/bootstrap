df6182020c6bf4e5195fcf99dfe91fc044c07cbd
knightoflorelei <knightoflorelei@gmail.com>
2022-09-25 09:03:43 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,16 @@
 sudo dnf update &&
 
 # Enable RPM Fusion repos
-sudo dnf install \ https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm &&
-sudo dnf install \ https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
+
+# Enable RPM Fusion AppStream metadata
+sudo dnf groupupdate core &&
 
 # Enable Flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
 
 # Install Gnome Tweaks
-sudo dnf install gnome-tweak &&
+sudo dnf install gnome-tweaks &&
 
 # Install neovim
 sudo dnf install neovim
