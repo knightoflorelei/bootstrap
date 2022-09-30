544cd260dbf1289ec80ff5e9cc2d1000a017fe77
knightoflorelei <knightoflorelei@gmail.com>
2022-09-30 08:53:54 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,8 @@ git config --global user.email "knightoflorelei@gmail.com"
 sudo dnf update &&
 
 # Enable RPM Fusion repos
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
 
 # Enable RPM Fusion AppStream metadata
 sudo dnf groupupdate core &&
@@ -34,8 +35,3 @@ chsh -s /usr/bin/zsh &&
 # Clone the dotfiles repo
 git clone https://github.com/knightoflorelei/dotfiles.git
 
-# Install Homebrew packages (will give command not found if the shell hasn't been reloaded)
-# brew install lf mpd mpv scrcpy powerlevel10k ranger zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting &&
-
-# Install flatpak applications (not need to d othis now)
-# flatpak install com.brave.Browser org.gnome.gitlab.somas.Apostrophe io.github.celluloid_player.Celluloid com.mattjakeman.ExtensionManager com.discordapp.Discord
